920bd55bf2a1fa5022b5b4e9d18268f1a40d8d56
Sai Kumar Bhimarasetty <64826210+saiwebpro@users.noreply.github.com>
2023-02-07 18:08:17 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import { Configuration, OpenAIApi } from 'openai';
 dotenv.config();
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey: process.env.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,
 });
 
 const openai = new OpenAIApi(configuration);
